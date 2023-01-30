1dd9af248284fd524a58c2face3bc9f9d4ab044a
SharonJenn <123975176+SharonJenn@users.noreply.github.com>
2023-01-30 23:04:34 +0200

    Guessing game my first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import random
+
+number = random.randint(1, 100)
+guess = None
+
+while guess != number:
+    guess = int(input("Guess the number between 1 and 100: "))
+    if guess < number:
+        print("Too low.")
+    elif guess > number:
+        print("Too high.")
+
+print("Correct!")
